e4da105bf1c66080f098a0e58eb98b053e110491
tstevens728 <tstevens728@yahoo.com>
2021-10-26 21:38:52 -0400

    finishing sign in handleSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.components";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import './sign-in.styles.scss';
 
@@ -17,11 +17,21 @@ class SignIn extends React.Component {
         };
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({email: '', password: ''})
-    }
+        const {email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+        
+    
 
     handleChange = event => {
         const {value, name} = event.target;
@@ -37,16 +47,17 @@ class SignIn extends React.Component {
 
                 <form onSubmit={this.handleSubmit}>
                     <FormInput 
-                        name="email"
+                        name='email'
                         label= 'email'
                         value={this.state.email} 
                         handleChange={this.handleChange}
                         required 
                     />
                     <FormInput
-                        name="password" 
+                        name='password' 
+                        type='password'
                         label='password'
-                        value={this.state.email}
+                        value={this.state.password}
                         handleChange={this.handleChange} 
                         required 
                     />
